98c0b9ab6b8322f0fac1eac56469ac7d69ba49b9
Karl Behrens <karlbehrensg@gmail.com>
2022-01-29 01:43:06 +0000

    Use router for post and user

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 # pylint: disable=broad-except, invalid-name
-from typing import List
 import time
 
 import psycopg2
-from fastapi import FastAPI, HTTPException, Response, status, Depends
+from fastapi import FastAPI
 from psycopg2.extras import RealDictCursor
-from sqlalchemy.orm import Session
 
-from . import models, schemas, utils
-from .database import get_db, engine
+from . import models
+from .database import engine
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -33,103 +32,5 @@ while True:
         time.sleep(3)
 
 
-my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
-    {"title": "favorite foods", "content": "I like pizza", "id": 2},
-]
-
-
-def find_post(post_id):
-    for post in my_posts:
-        if post["id"] == post_id:
-            return post
-    return None
-
-
-def find_index_post(post_id):
-    for i, post in enumerate(my_posts):
-        if post["id"] == post_id:
-            return i
-    return None
-
-
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    new_post = models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    return new_post
-
-
-@app.get("/posts/{post_id}", response_model=schemas.Post)
-def get_post(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id).first()
-    if post is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id {post_id} not found",
-        )
-    return post
-
-
-@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id)
-    if post.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id {post_id} not found",
-        )
-    post.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{post_id}", response_model=schemas.Post)
-def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    post_query = db.query(models.Post).filter(models.Post.id == post_id)
-
-    if post_query.first() is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with id {post_id} not found",
-        )
-
-    post_query.update(post.dict(), synchronize_session=False)
-    db.commit()
-
-    return post_query.first()
-
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # hash the password - user.passowd
-    user.password = utils.hash_str(user.password)
-    new_user = models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-@app.get("/users/{user_id}", response_model=schemas.UserOut)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User {user_id} not found",
-        )
-    return user
+app.include_router(post.router)
+app.include_router(user.router)
---
+++ app/routers/user.py
@@ -0,0 +1,32 @@
+# pylint: disable=broad-except, invalid-name
+from fastapi import HTTPException, status, Depends, APIRouter
+from sqlalchemy.orm import Session
+
+from .. import models, schemas, utils
+from ..database import get_db
+
+router = APIRouter()
+
+
+@router.post(
+    "/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
+)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    # hash the password - user.passowd
+    user.password = utils.hash_str(user.password)
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/users/{user_id}", response_model=schemas.UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User {user_id} not found",
+        )
+    return user
